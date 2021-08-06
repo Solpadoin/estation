edea8c1783bfe5c36df1cb118bfe6127c4d97c5e
bohdan.semesko <bohdan.semesko@ionidea.com>
2021-08-06 12:42:59 +0300

    Added Scribe Documentation Generator

--- app/Http/Controllers/Api/EstationController.php
+++ app/Http/Controllers/Api/EstationController.php
@@ -16,38 +16,61 @@ class EstationController extends Controller
         $this->service = $service;
     }
 
+    /**
+     * @group RESTFul API
+     */
     public function store(StationRequestForm $request)
     {
         return response()->json($this->service->create($request->validated()));
     }
 
+    /**
+     * @group Version
+     */
     public function version()
     {
         return $this->service->version();
     }
 
+    /**
+     * @group RESTFul API
+     */
     public function update(StationRequestForm $request, $recordId)
     {
         $status = $this->service->update($recordId, $request->all());
         return response()->json(['status' => $status], 200);
     }
 
+    /**
+     * @group RESTFul API
+     */
     public function destroy(StationRequestForm $request, $id)
     {
         return response()->json(['status' => $this->service->delete($id)], 201);
     }
 
+    /**
+     * @group RESTFul API
+     */
     public function index()
     {
         return response()->json(['items' => $this->service->get()], 200);
     }
 
+    /**
+     * @group RESTFul API
+     */
     public function show(Request $request, $id)
     {
         $request['id'] = $id;
         return $this->search($request);
     }
 
+    /**
+     * @bodyParam radius float Radius. Example: 40
+     * @bodyParam longitude float Longitude. Example: 34
+     * @bodyParam latitude float Latitude. Example: 76
+     */
     public function closest(Request $request)
     {
         $params = $request->all();
@@ -59,6 +82,9 @@ class EstationController extends Controller
         return response()->json(['success' => true, 'items' => $elements], 200);
     }
 
+    /**
+     * @group Search Item
+     */
     public function search(Request $request)
     {
         $payload  = $request->all();
